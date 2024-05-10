dc55c515491d10cd9ed3a72981e047d2770af70e
EMİRHAN KARAKOÇ <emirhankarakoc@yahoo.com>
2024-05-10 05:40:36 +0300

    init

---
+++ backend/src/main/java/com/karakocsoftware/Uptime/robot/project/UptimeRobotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.karakocsoftware.Uptime.robot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeRobotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimeRobotProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/karakocsoftware/Uptime/robot/project/UptimeRobotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karakocsoftware.Uptime.robot.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeRobotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
